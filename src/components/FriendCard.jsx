ab80e91ab3ee19ca76b649fe49641a9078f72e73
Habib <habibullahkhandokar01@gmail.com>
2026-04-17 16:56:23 +0600

    friendcard and navbar updated

--- src/components/FriendCard.jsx
+++ src/components/FriendCard.jsx
@@ -2,46 +2,41 @@ import { Link } from "react-router-dom";
 
 const FriendCard = ({ friend }) => {
   const statusStyle = {
-    overdue: "bg-red-100 border-red-400",
-    "almost due": "bg-yellow-100 border-yellow-400",
-    "on-track": "bg-green-100 border-green-400",
+    overdue: "bg-red-500",
+    "almost due": "bg-yellow-500",
+    "on-track": "bg-green-500",
   };
 
   return (
     <Link to={`/friend/${friend.id}`}>
-      <div
-        className={`p-4 rounded-xl border-2 shadow hover:shadow-lg transition ${statusStyle[friend.status]}`}
-      >
-        {/* Image */}
+      <div className="shadow-md rounded-sm p-5 flex flex-col items-center gap-2">
         <img
           src={friend.picture}
           alt={friend.name}
-          className="w-full h-40 object-cover rounded-lg mb-3"
+          className="w-30 h-30 rounded-full object-cover"
         />
 
-        {/* Name */}
         <h2 className="text-lg font-semibold">{friend.name}</h2>
 
-        {/* Days */}
         <p className="text-sm text-gray-600">
-          📅 {friend.days_since_contact} days ago
+          {friend.days_since_contact}d ago
         </p>
 
-        {/* Tags */}
         <div className="flex flex-wrap gap-2 mt-2">
           {friend.tags.map((tag, index) => (
             <span
               key={index}
-              className="text-xs bg-gray-200 px-2 py-1 rounded-full"
+              className="text-xs bg-green-200 px-2 py-1 rounded-full"
             >
-              #{tag}
+              {tag}
             </span>
           ))}
         </div>
 
-        {/* Status */}
-        <p className="mt-2 text-sm font-medium capitalize">
-          🔴 {friend.status}
+        <p
+          className={`rounded-xl border-2 transition w-fit text-white px-2 ${statusStyle[friend.status]}`}
+        >
+          {friend.status}
         </p>
       </div>
     </Link>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = () => {
       <h1 className="font-bold text-xl">Keen<span className="font-normal">Keeper</span></h1>
 
       <div className="flex gap-6">
-        <NavLink to="/" className={({ isActive }) => isActive ? "bg-green-800 text-white p-2 rounded-sm" : "text-green-80 p-2"}>
+        <NavLink to="/" className={({ isActive }) => isActive ? "bg-green-800 text-white px-2 rounded-sm" : "text-green-80 px-2"}>
           <FaHome className="inline mr-1" /> Home
         </NavLink>
 
-        <NavLink to="/timeline" className={({ isActive }) => isActive ? "bg-green-800 text-white p-2 rounded-sm" : "text-green-80 p-2"}>
+        <NavLink to="/timeline" className={({ isActive }) => isActive ? "bg-green-800 text-white px-2 rounded-sm" : "text-green-80 px-2"}>
           <FaClock className="inline mr-1" /> Timeline
         </NavLink>
 
-        <NavLink to="/stats" className={({ isActive }) => isActive ? "bg-green-800 text-white p-2 rounded-sm" : "text-green-80 p-2"}>
+        <NavLink to="/stats" className={({ isActive }) => isActive ? "bg-green-800 text-white px-2 rounded-sm" : "text-green-80 px-2"}>
           <FaChartPie className="inline mr-1" /> Stats
         </NavLink>
       </div>
